b6e1fbd23c19f322cab9dfb4ad991acba41c933d
jeffbaldwin <jjbaldwin7@gmail.com>
2017-03-09 13:19:15 -0800

    Updated nav to hide/show on mobile

--- js/script.js
+++ js/script.js
@@ -1,37 +1,17 @@
-
-
-/* 
-
-JS code partly taken from: 
-
-http://stackoverflow.com/questions/13274592/leave-menu-bar-fixed-on-top-when-scrolled
-
-Answer March 4 2013 by Tomas Gonzalez 
-
-ORIGINAL CODE
-
-$("document").ready(function($){
-    var nav = $('#main-menu-container');
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 125) {
-            nav.addClass("f-nav");
-        } else {
-            nav.removeClass("f-nav");
-        }
-    });
-});
-
-*/
+'use strict';
 
 
 
 $("document").ready(function($){
     
-    
-    
-    
-    /* Stick nav to top when scrolling */
+    /* Stick nav to top when scrolling 
+
+    JS code partly taken from: 
+
+    http://stackoverflow.com/questions/13274592/leave-menu-bar-fixed-on-top-when-scrolled
+
+    Answer March 4 2013 by Tomas Gonzalez */
+
     var nav = $('#nav');
 
     $(window).scroll(function () {
@@ -42,13 +22,16 @@ $("document").ready(function($){
             nav.removeClass("fix_nav");
         }
     });
-    /**/
+    
+    /* -------------------- */
     
     
-    /* 
-    Product image hover zoom + pan 
-    Code partially from: https://codepen.io/ccrch/pen/yyaraz
-    */
+    /* Product image hover zoom + pan 
+    
+    Code partially from: 
+    
+    https://codepen.io/ccrch/pen/yyaraz */
+    
     $('.product_detail_image_inner_container')
         .on('mouseover', function(){
           $(this).children('.product_detail_image').css({'transform': 'scale(2)'});
@@ -60,6 +43,10 @@ $("document").ready(function($){
           $(this).children('.product_detail_image').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
         })
     
+    /* -------------------- */
+    
+    
+    /* Show more text in updates */
     
     $('.update_show_more').on('click', function(){
         $('.update_copy').children('.update_show_more').addClass("update_hidden");
@@ -67,18 +54,60 @@ $("document").ready(function($){
         $('.update_copy').children('.update_hidden_copy').removeClass("update_hidden");
     })
     
+    /* -------------------- */
+    
+    
+    /* Proceed to checkout link */
     
     $('#proceed_to_checkout').on('click', function(){
         window.location.href = "purchase.html";
     })
+    
+    /* -------------------- */
     
     
 //    button.addEventListener('click', function() {
 //        document.querySelector('ul').classList.toggle('hidden');
 //    })
 
+    
+    
+    
+    
+    
+    /* Hide/show nav */
+    
+    /* If the window is smaller than 700px, hide that nav buttons and show the NAV button */
+    if ($(window).width() < 700) {
+        $('#nav_button').removeClass('nav_hidden');
+        $('#nav_items').addClass('nav_hidden');
+//        $('#logo_small').addClass('nav_hidden');
+    } else {
+        $('#nav_button').addClass('nav_hidden');
+        $('#nav_items').removeClass('nav_hidden');
+//        $('#logo_small').removeClass('nav_hidden');
+    }
+    
+    /* When the NAV button is clicked */
+    $('#nav_toggle').click( function() { 
+        
+        console.log('click');
+        /* Show the other nav buttons */
+        $('#nav_items').toggleClass('nav_hidden');
+        $('#nav_toggle').toggleClass('close');
+    
+        if ($(this).hasClass("close")) {
+            $(this).html("CLOSE");
+        } else {
+            $(this).html("NAV");
+        }
+        
+    });
+        
+    /* -------------------- */
+    
+    
 });
-
 
 
 
